6da438c3211e6382223ccd4a4b2e68b31167c74c
Nyikwagh Moses <mosnyik@gmail.com>
2026-06-20 06:51:47 +0100

    feat: added the tawk.to chatbot widget

--- components/TawkTo.tsx
+++ components/TawkTo.tsx
@@ -10,6 +10,12 @@ export default function TawkTo() {
   return (
     <Script id="tawk-to" strategy="lazyOnload">{`
       var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      Tawk_API.customStyle = {
+        visibility: {
+          desktop: { position: 'br', xOffset: 24, yOffset: 24 },
+          mobile: { position: 'br', xOffset: 16, yOffset: 16 }
+        }
+      };
       (function(){
         var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
         s1.async=true; s1.src='${src}'; s1.charset='UTF-8';
